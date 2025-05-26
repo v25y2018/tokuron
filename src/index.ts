690b0e43b9086e55e11196b8cbfe20cc378c45e5
v25y2018 <v25y2018@oita-u.ac.jp>
2025-05-26 07:14:34 +0000

    honoでOpenAPIの確認

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Hono } from 'hono'
+import { logger} from 'hono/logger'
+import { zValidator } from '@hono/zod-validator'
+import { z, createRoute, OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui'
 
 const app = new Hono()
+
+app.use(logger())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -9,5 +15,15 @@ app.get('/api', (c) => {
   const query=c.req.query('name');
   return c.json({message: `Hello ${query}!`})
 })
+
+app.post('/api',
+  zValidator('form', z.object({body: z.string()})),
+  (c) => {
+    const validated = c.req.valid('form')
+    return c.text('POST /api')
+  }
+)
+
+app.notFound((c) => c.text('Custom 404 Message', 404))
 
 export default app
