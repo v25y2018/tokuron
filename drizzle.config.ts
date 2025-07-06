35af6d5d2398b0db62241bc099fcad8f86593836
v25y2018 <v25y2018@oita-u.ac.jp>
2025-07-06 13:02:51 +0000

    データベースとORM

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "drizzle-kit";
+import { Schema } from "zod";
+
+export default {
+    schema: "./src/schema.ts",
+    out: "./migrations",
+    driver: "d1-http",
+    dialect: "sqlite",
+    dbCredentials: {
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID as string,
+        databaseId: process.env.CLOUDFLARE_DATABASE_ID as string,
+        token: process.env.CLOUDFLARE_D1_TOKEN as string 
+    }
+} satisfies Config;
